3d3dffb3f7c58db6f82e2e5ec43cccf64b4f0dc1
Aaron Davidson <aaron@databricks.com>
2014-11-06 17:20:59 -0800

    Test with encoders

--- network/shuffle/src/main/java/org/apache/spark/network/shuffle/ExternalShuffleMessages.java
+++ network/shuffle/src/main/java/org/apache/spark/network/shuffle/ExternalShuffleMessages.java
@@ -20,13 +20,49 @@ package org.apache.spark.network.shuffle;
 import java.io.Serializable;
 import java.util.Arrays;
 
+import com.google.common.base.Charsets;
 import com.google.common.base.Objects;
+import io.netty.buffer.ByteBuf;
+
+import org.apache.spark.network.protocol.Encodable;
+import org.apache.spark.network.protocol.Encoders;
 
 /** Messages handled by the {@link ExternalShuffleBlockHandler}. */
 public class ExternalShuffleMessages {
 
+  public interface ExternalShuffleMessage extends Encodable {
+    Type type();
+
+    /** Preceding every serialized Message is its type, which allows us to deserialize it. */
+    public static enum Type implements Encodable {
+      OpenShuffleBlocks(0), RegisterExecutor(1);
+
+      private final byte id;
+
+      private Type(int id) {
+        assert id < 128 : "Cannot have more than 128 message types";
+        this.id = (byte) id;
+      }
+
+      public byte id() { return id; }
+
+      @Override public int encodedLength() { return 1; }
+
+      @Override public void encode(ByteBuf buf) { buf.writeByte(id); }
+
+      public static Type decode(ByteBuf buf) {
+        byte id = buf.readByte();
+        switch (id) {
+          case 0: return OpenShuffleBlocks;
+          case 1: return RegisterExecutor;
+          default: throw new IllegalArgumentException("Unknown message type: " + id);
+        }
+      }
+    }
+  }
+
   /** Request to read a set of shuffle blocks. Returns [[ShuffleStreamHandle]]. */
-  public static class OpenShuffleBlocks implements Serializable {
+  public static class OpenShuffleBlocks implements ExternalShuffleMessage {
     public final String appId;
     public final String execId;
     public final String[] blockIds;
@@ -36,6 +72,9 @@ public class ExternalShuffleMessages {
       this.execId = execId;
       this.blockIds = blockIds;
     }
+
+    @Override
+    public Type type() { return Type.OpenShuffleBlocks; }
 
     @Override
     public int hashCode() {
@@ -61,10 +100,31 @@ public class ExternalShuffleMessages {
       }
       return false;
     }
+
+    @Override
+    public int encodedLength() {
+      return Encoders.Strings.encodedLength(appId)
+        + Encoders.Strings.encodedLength(execId)
+        + Encoders.StringArrays.encodedLength(blockIds);
+    }
+
+    @Override
+    public void encode(ByteBuf buf) {
+      Encoders.Strings.encode(buf, appId);
+      Encoders.Strings.encode(buf, execId);
+      Encoders.StringArrays.encode(buf, blockIds);
+    }
+
+    public static OpenShuffleBlocks decode(ByteBuf buf) {
+      String appId = Encoders.Strings.decode(buf);
+      String execId = Encoders.Strings.decode(buf);
+      String[] blockIds = Encoders.StringArrays.decode(buf);
+      return new OpenShuffleBlocks(appId, execId, blockIds);
+    }
   }
 
   /** Initial registration message between an executor and its local shuffle server. */
-  public static class RegisterExecutor implements Serializable {
+  public static class RegisterExecutor implements ExternalShuffleMessage {
     public final String appId;
     public final String execId;
     public final ExecutorShuffleInfo executorInfo;
@@ -77,6 +137,9 @@ public class ExternalShuffleMessages {
       this.execId = execId;
       this.executorInfo = executorInfo;
     }
+
+    @Override
+    public Type type() { return Type.RegisterExecutor; }
 
     @Override
     public int hashCode() {
@@ -101,6 +164,27 @@ public class ExternalShuffleMessages {
           && Objects.equal(executorInfo, o.executorInfo);
       }
       return false;
+    }
+
+    @Override
+    public int encodedLength() {
+      return Encoders.Strings.encodedLength(appId)
+        + Encoders.Strings.encodedLength(execId)
+        + executorInfo.encodedLength();
+    }
+
+    @Override
+    public void encode(ByteBuf buf) {
+      Encoders.Strings.encode(buf, appId);
+      Encoders.Strings.encode(buf, execId);
+      executorInfo.encode(buf);
+    }
+
+    public static RegisterExecutor decode(ByteBuf buf) {
+      String appId = Encoders.Strings.decode(buf);
+      String execId = Encoders.Strings.decode(buf);
+      ExecutorShuffleInfo executorShuffleInfo = ExecutorShuffleInfo.decode(buf);
+      return new RegisterExecutor(appId, execId, executorShuffleInfo);
     }
   }
 }
